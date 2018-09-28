81cca2e94ec7394b3e73e8228eaf26018e1c22b8
Magusinnnno <u1940026@campus.udg.edu>
2018-09-28 18:58:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 		<title>Sign in with your account and password</title>
 	</head>
 	<body>
+		<form>
+			First name:<br>
+			<input type="text" name="firstname"><br>
+			Last name:<br>
+			<input type="text" name="lastname">
+		</form>
 		<?php
 	    		
 		?>
+		</form>
 	</body>
 </html> 
