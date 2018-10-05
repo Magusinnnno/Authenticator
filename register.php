1db39af17c143546a87f5a868f18a00cb0f830ec
Magusinnnno <u1940026@campus.udg.edu>
2018-10-05 18:14:42 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Register a new account</title>
-  </head>
-  <body>
-    <?php
-      
-    ?>
-  </body>
-</html>
+	<head>
+		<title>Register a new account</title>
+	</head>
+	<body>
+		<form>
+			First name:<br>
+			<input type="text" name="firstname"><br>
+			Last name:<br>
+			<input type="text" name="lastname"><br>
+			Email:<br>
+			<input type="text" name="email"><br>
+		</form>
+		<?php
+	    		
+		?>
+		</form>
+	</body>
+</html> 
