1ca9d50e7b25f11f41ef938638a200ae06e2d1c9
Magusinnnno <u1940026@campus.udg.edu>
2018-10-05 18:51:43 +0200

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,21 @@
 		<title>Register a new account</title>
 	</head>
 	<body>
+		<?php>
+		try {
+		    $conn = new PDO("sqlsrv:server = tcp:gddv.database.windows.net,1433; Database = sm", "sm", "{your_password_here}");
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $e) {
+		    print("Error connecting to SQL Server.");
+		    die(print_r($e));
+		}
+
+		// SQL Server Extension Sample Code:
+		$connectionInfo = array("UID" => "sm@gddv", "pwd" => "{your_password_here}", "Database" => "sm", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+		$serverName = "tcp:gddv.database.windows.net,1433";
+		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		?>
 		<form>
 			First name:<br>
 			<input type="text" name="firstname"><br>
