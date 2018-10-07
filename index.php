61139cf3db5c8e4087eb0a6e1e35bdf3e4fee424
Paolite <34453363+Paolite@users.noreply.github.com>
2018-10-07 23:07:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<style>
+			.error {color: #FF0000;}
+		</style>
+	</head>
+	<?php
+		$userErr = $passErr = "";
+		$user = $pass = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (empty($_POST["user"])) {
+				$userErr = "User camp is empty";
+			} else {
+				$user = test_input($_POST["user"]);
+			}
+			if (empty($_POST["pass"])) {
+				$passErr = "Password camp is empty";
+			} else {
+				$pass = test_input($_POST["pass"]);
+			}
+		}
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+	?>
+	<head>
 		<title>Sign in with your account and password</title>
 	</head>
 	<body>
-		<form>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 			Username:<br>
-			<input type="text" name="firstname"><br>
+			<input type="text" name="user"><br>
 			Password:<br>
-			<input type="text" name="lastname"><br>
+			<input type="text" name="pass"><br>
+			<input type="submit" name="submit" value="Submit">  
 		</form>
-		<?php
-	    		
-		?>
 	</body>
 </html> 
