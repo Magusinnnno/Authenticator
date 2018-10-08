3abe2fbf1c29079cc77a7f0ebcd9a2543e8a4461
Paolite <34453363+Paolite@users.noreply.github.com>
2018-10-08 02:05:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<style>
-			.error {color: #FF0000;}
-		</style>
-	</head>
-	<?php
-		$userErr = $passErr = "";
-		$user = $pass = "";
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			if (empty($_POST["user"])) {
-				$userErr = "User camp is empty";
-			} else {
-				$user = test_input($_POST["user"]);
-			}
-			if (empty($_POST["pass"])) {
-				$passErr = "Password camp is empty";
-			} else {
-				$pass = test_input($_POST["pass"]);
-			}
-		}
-		function test_input($data) {
-			$data = trim($data);
-			$data = stripslashes($data);
-			$data = htmlspecialchars($data);
-			return $data;
-		}
-	?>
-	<head>
 		<title>Sign in with your account and password</title>
 	</head>
 	<body>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<form method="post" action="login.php">
 
 			<div class="container">
 				<label for="uname"><b>Username</b></label>
