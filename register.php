c262b0f16a7071d8b89645219d77a15b89153eeb
Magusinnnno <u1940026@campus.udg.edu>
2018-10-08 02:07:51 +0200

    Update register.php

--- register.php
+++ register.php
@@ -13,12 +13,21 @@ echo '<!DOCTYPE html>
 			<input type="text" name="firstname"><br>
 			Last name:<br>
 			<input type="text" name="lastname"><br>
+			Age:<br>
+			<input type="number" name="age" min="12" max="120"><br>
 			Email:<br>
 			<input type="text" name="email"><br>
 			Nickname: <br>
 			<input type="text" name="nick"><br>
 			Password: <br>
 			<input type="password" name="password"><br>
+			<ul>
+				<li>Password must have a minimum of 8 characters</li>
+				<li>Password must include at least one number</li>
+				<li>Password must include at least one uppercase word</li>
+				<li>Password must include at least one lowercase word</li>
+			</ul>  
+			<br>
 			<input type="submit" value="Register" />
 		</form>
 		<hr>
