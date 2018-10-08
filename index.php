e828269c425386180f449d7ed2b606b5647c9f57
Paolite <34453363+Paolite@users.noreply.github.com>
2018-10-08 03:00:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html>
+	<?php
+		$cookie_name_user = "user";
+		$cookie_name_pass = "pass";
+		$user = $pass = "";
+		if(isset($_COOKIE[$cookie_name_user])) {
+			$user = $_COOKIE[$cookie_name_user];
+		}
+		if(isset($_COOKIE[$cookie_name_pass])) {
+			$pass = $_COOKIE[$cookie_name_pass];
+		}
+	?>
 	<head>
 		<title>Sign in with your account and password</title>
 	</head>
@@ -8,10 +19,10 @@
 
 			<div class="container">
 				<label for="uname"><b>Username</b></label>
-				<input type="text" placeholder="Enter Username" name="user" required>
+				<input type="text" value="<?php echo $user; ?>" placeholder="Enter Username" name="uname" required>
 
 				<label for="psw"><b>Password</b></label>
-				<input type="password" placeholder="Enter Password" name="pass" required>
+				<input type="password" value="<?php echo $pass; ?>" placeholder="Enter Password" name="psw" required>
 
 				<button type="submit">Login</button>
 			</div>
